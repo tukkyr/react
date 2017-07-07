876a54df560c84d02bd01aaa5a5a1d84d0723fa7
Shota Ueda <shota-ueda@apn.alpine.co.jp>
2017-07-07 01:40:58 +0000

    template use googlemap api

--- app.js
+++ app.js
@@ -1,67 +1,82 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
+class GMap extends React.Component {
+  constructor() {
+    super();
+    this.state={
+      zoom: 10,
+    }
   }
-}
-
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />;
+  static propTypes() {
+    initialCenter: React.PropTypes.objectOf(React.PropTypes.number).isRequired
   }
 
   render() {
-    const status = 'Next player: X';
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+    return <div className="GMap">
+      <div className='UpdatedText'>
+        <p>Current Zoom: this.state.zoom</p>
       </div>
-    );
-  }
-}
-
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
+      <div className='GMap-canvas' ref="mapCanvas">
       </div>
-    );
+    </div>
+  }
+
+  componentDidMount() {
+    // create the map, marker and infoWindow after the component has
+    // been rendered because we need to manipulate the DOM for Google =(
+    this.map = this.createMap()
+    this.marker = this.createMarker()
+    this.infoWindow = this.createInfoWindow()
+  
+    // have to define google maps event listeners here too
+    // because we can't add listeners on the map until its created
+    google.maps.event.addListener(this.map, 'zoom_changed', ()=> this.handleZoomChange())
+  }
+
+  // clean up event listeners when component unmounts
+  componentDidUnMount() {
+    google.maps.event.clearListeners(map, 'zoom_changed')
+  }
+
+  createMap() {
+    let mapOptions = {
+      zoom: this.state.zoom,
+      center: this.mapCenter()
+    }
+    return new google.maps.Map(this.refs.mapCanvas, mapOptions)
+  }
+
+  mapCenter() {
+    return new google.maps.LatLng(
+      this.props.initialCenter.lat,
+      this.props.initialCenter.lng
+    )
+  }
+
+  createMarker() {
+    return new google.maps.Marker({
+      position: this.mapCenter(),
+      map: this.map
+    })
+  }
+
+  createInfoWindow() {
+    let contentString = "<div class='InfoWindow'>I'm a Window that contains Info Yay</div>"
+    return new google.maps.InfoWindow({
+      map: this.map,
+      anchor: this.marker,
+      content: contentString
+    })
+  }
+  
+  handleZoomChange() {
+    this.setState({
+      zoom: this.map.getZoom()
+    })
   }
 }
 
-// ========================================
+var initialCenter = { lng: -90.1056957, lat: 29.9717272 }
 
-ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
-);
-
+ReactDOM.render(<GMap initialCenter={initialCenter} />, document.getElementById('map'));
